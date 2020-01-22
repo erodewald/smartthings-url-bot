bba61478de51cbcf35c089403c4923688918fe63
Eric Rodewald <eric.rode@gmail.com>
2020-01-21 22:26:39 -0600

    Strip out debug replies; send exp. slack block

--- bots/dispatchBot.js
+++ bots/dispatchBot.js
@@ -1,12 +1,16 @@
+
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler } = require('botbuilder');
+const { TurnContext, ActivityTypes, ActivityHandler } = require('botbuilder');
 const { LuisRecognizer, QnAMaker } = require('botbuilder-ai');
 
 class DispatchBot extends ActivityHandler {
-    constructor() {
+    constructor(qnaAppInsightsClient) {
         super();
+
+        // Add ApplicationInsights as the telemetry client
+        this.telemetryClient = qnaAppInsightsClient;
 
         // If the includeApiResults parameter is set to true, as shown below, the full response
         // from the LUIS api will be made available in the properties  of the RecognizerResult
@@ -22,13 +26,18 @@ class DispatchBot extends ActivityHandler {
             knowledgeBaseId: process.env.QnAKnowledgebaseId,
             endpointKey: process.env.QnAEndpointKey,
             host: process.env.QnAEndpointHostName
-        });
+        }, this.telemetryClient);
 
         this.dispatchRecognizer = dispatchRecognizer;
         this.qnaMaker = qnaMaker;
 
         this.onMessage(async (context, next) => {
             console.log('Processing Message Activity.');
+
+            const reference = TurnContext.getConversationReference(context.activity);
+            const activity = TurnContext.applyConversationReference({ type: 'typing' }, reference);
+            await context.adapter.sendActivities(context, [activity]);
+            // await context.sendActivity({ type: ActivityTypes.Typing });
 
             // First, we use the dispatch model to determine which cognitive service (LUIS or QnA) to use.
             const recognizerResult = await this.dispatchRecognizer.recognize(context);
@@ -54,7 +63,6 @@ class DispatchBot extends ActivityHandler {
             break;
         default:
             console.log(`Dispatch unrecognized intent: ${ intent }.`);
-            await context.sendActivity(`Dispatch unrecognized intent: ${ intent }.`);
             break;
         }
     }
@@ -66,26 +74,87 @@ class DispatchBot extends ActivityHandler {
         const result = luisResult.connectedServiceResult;
         const intent = result.topScoringIntent.intent;
 
-        console.log(`OfficeAutomation top intent ${ intent }.`);
-        console.log(`OfficeAutomation intents detected:  ${ luisResult.intents.map((intentObj) => intentObj.intent).join('\n\n') }.`);
+        console.log(`OfficeAutomation top intent ${intent}.`);
+        console.log(`OfficeAutomation intents detected:  ${luisResult.intents.map((intentObj) => intentObj.intent).join('\n\n')}.`);
         
-        await context.sendActivity(`OfficeAutomation top intent ${intent}.`);
-        await context.sendActivity(`OfficeAutomation intents detected:  ${ luisResult.intents.map((intentObj) => intentObj.intent).join('\n\n') }.`);
+        let blocks = {
+            blocks:
+                [
+                    {
+                        type: "section",
+                        text: {
+                            type: "mrkdwn",
+                            text: "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*"
+                        }
+                    },
+                    {
+                        type: "section",
+                        fields: [
+                            {
+                                type: "mrkdwn",
+                                text: "*Type:*\nComputer (laptop)"
+                            },
+                            {
+                                type: "mrkdwn",
+                                text: "*When:*\nSubmitted Aut 10"
+                            },
+                            {
+                                type: "mrkdwn",
+                                text: "*Last Update:*\nMar 10, 2015 (3 years, 5 months)"
+                            },
+                            {
+                                type: "mrkdwn",
+                                text: "*Reason:*\nAll vowel keys aren't working."
+                            },
+                            {
+                                type: "mrkdwn",
+                                text: "*Specs:*\n\"Cheetah Pro 15\" - Fast, really fast\""
+                            }
+                        ]
+                    },
+                    {
+                        type: "actions",
+                        elements: [
+                            {
+                                type: "button",
+                                text: {
+                                    type: "plain_text",
+                                    emoji: true,
+                                    text: "Approve"
+                                },
+                                style: "primary",
+                                value: "click_me_123"
+                            },
+                            {
+                                type: "button",
+                                text: {
+                                    type: "plain_text",
+                                    emoji: true,
+                                    text: "Deny"
+                                },
+                                style: "danger",
+                                value: "click_me_123"
+                            }
+                        ]
+                    }
+                ]
+        };
 
-        if (luisResult.entities.length > 0) {
-            await context.sendActivity(`OfficeAutomation entities were found in the message: ${ luisResult.entities.map((entityObj) => entityObj.entity).join('\n\n') }.`);
-        }
+        await context.sendActivity({ type: ActivityTypes.Message, channelData: blocks });
+        // await context.sendActivity(`OfficeAutomation top intent ${intent}.`);
+        // await context.sendActivity(`OfficeAutomation intents detected:  ${ luisResult.intents.map((intentObj) => intentObj.intent).join('\n\n') }.`);
+
+        // if (luisResult.entities.length > 0) {
+        //     await context.sendActivity(`OfficeAutomation entities were found in the message: ${ luisResult.entities.map((entityObj) => entityObj.entity).join('\n\n') }.`);
+        // }
     }
 
     async processChitChat(context) {
         console.log('processSampleQnA');
 
         const results = await this.qnaMaker.getAnswers(context);
-
         if (results.length > 0) {
             await context.sendActivity(`${ results[0].answer }`);
-        } else {
-            await context.sendActivity('Sorry, could not find an answer in the Q and A system.');
         }
     }
 }
